6d14ea56bcbfe89bec59bf77c928f018fe1ade63
santoshmondal <santosh.ece06@gmail.com>
2023-03-25 20:03:56 +0530

    event binding.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 function App() {
-  // Data
+  // Data Variabl :: Modal
+  let btnTitle = "Save Me";
   let title = "My Project";
 
-  // VIEW
+  let saveMe = () => {
+    console.log("SAVE ME");
+  };
+
   return (
     <div>
       <h1>My View Areaaa</h1>
       <h1 id={title}> {title} </h1>
+      <div name={title}>{title}</div>
+
+      <input type="button" value={btnTitle} onClick={saveMe} />
     </div>
   );
 }
