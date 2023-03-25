64135dfd5b078f936e792e1589a951d3c4fad3c5
santoshmondal <santosh.ece06@gmail.com>
2023-03-25 19:49:57 +0530

    parent tag and child tag.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-// JSX XML
+// JSX - XML :: ROOT ELEMENT
+// THere can be only one Parent tag
 function App() {
   return (
     <div>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur,
-      doloremque reiciendis distinctio unde sit, quisquam debitis esse
-      praesentium ipsum eveniet dolorum et? Aliquid exercitationem animi
-      voluptates, eveniet assumenda accusamus dicta!
+      <h1>Hello</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, sequi!
+        Veritatis aperiam rerum minima voluptatibus dicta dolore modi eaque
+        repellat? Blanditiis eius odit, est laborum ratione ipsam ullam! Labore,
+        quisquam.
+      </p>
     </div>
   );
 }
