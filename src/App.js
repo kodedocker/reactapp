c02fa9355e55c2cf29472cfbc21b42827885534c
santoshmondal <santosh.ece06@gmail.com>
2023-03-25 19:51:48 +0530

    jsx in variable.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,7 @@
-// JSX - XML :: ROOT ELEMENT
-// THere can be only one Parent tag
 function App() {
-  return (
-    <div>
-      <h1>Hello</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, sequi!
-        Veritatis aperiam rerum minima voluptatibus dicta dolore modi eaque
-        repellat? Blanditiis eius odit, est laborum ratione ipsam ullam! Labore,
-        quisquam.
-      </p>
-    </div>
-  );
+  let a = 1;
+  let ref = <h1>Hello</h1>;
+  return ref;
 }
 
 export default App;
