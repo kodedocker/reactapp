f619e81d5b90e71835f6882d3f40233df8859a3a
santoshmondal <santosh.ece06@gmail.com>
2023-03-26 11:45:59 +0530

    workig wih input.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,37 @@ import { useRef, useState } from "react";
 function App() {
   return (
     <div>
+      <HelloInputEvent3 />
       <HelloInputEvent2 />
       <HelloInputEvent1 />
+    </div>
+  );
+}
+
+function HelloInputEvent3() {
+  let [message, setMessage] = useState("");
+
+  let messageHandler = (e) => {
+    setMessage(e.target.value);
+  };
+
+  let saveMe = () => {
+    // API CALL
+  };
+
+  return (
+    <div>
+      <h1>Option2 + Event Binding</h1>
+      <input
+        type="text"
+        placeholder="Enter here...."
+        value={message}
+        onChange={messageHandler}
+      />
+      <input type="button" value="Save" onClick={saveMe} />
+
+      <div>{message}</div>
+      <hr />
     </div>
   );
 }
