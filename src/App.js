60df5fb7f43e002e37836e7b3c97dbcbc77706ea
santoshmondal <santosh.ece06@gmail.com>
2023-03-26 17:53:35 +0530

    list assignment.

--- src/App.js
+++ src/App.js
@@ -4,8 +4,55 @@ function App() {
   return (
     <div>
       <h1>List Assignment</h1>
+      <ListDemo2 />
       <ListDemo1 />
       <ListDemo />
+    </div>
+  );
+}
+
+function ListDemo2() {
+  let [city, setCity] = useState("");
+  let [list, setList] = useState(["delhi", "calcutta"]);
+
+  const inputCityHandler = (e) => {
+    setCity(e.target.value);
+  };
+
+  const inputSubmitHandler = (e) => {
+    if (e.keyCode == 13) {
+      addNewCity();
+    }
+  };
+
+  const addNewCity = () => {
+    if (!city) {
+      return;
+    }
+
+    let newlist = [city, ...list];
+    setList(newlist);
+
+    // clear the box
+    setCity("");
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Enter city..."
+        value={city}
+        onChange={inputCityHandler}
+        onKeyUp={inputSubmitHandler}
+      />
+      <input type="button" value="Add New City" onClick={addNewCity} />
+
+      <ul>
+        {list.map((item, index) => (
+          <li key={index}>2.{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
@@ -38,7 +85,7 @@ function ListDemo1() {
 
       <ul>
         {list.map((item, index) => (
-          <li key={index}>{item}</li>
+          <li key={index}>1.{item}</li>
         ))}
       </ul>
     </div>
