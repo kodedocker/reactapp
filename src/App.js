8eef9fde38dbb627357ffd48c84c497d465ba1d3
santoshmondal <santosh.ece06@gmail.com>
2023-03-26 11:22:27 +0530

    event. handlign react.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import { useState } from "react";
-
 function App() {
-  let counter1 = 0;
-  let [counter, setCounter] = useState(0);
+  let project = "react wod";
 
-  let handleIncrment = () => {
-    counter1++;
-    console.log(counter1);
+  let handleClick1 = () => {
+    console.log("Click 1");
+  };
 
-    setCounter(counter++);
+  // e -> this is optinal
+  let handleClick2 = (e) => {
+    console.log("Click 2", e);
+  };
+
+  // p1 - use defined, if userd pass this as arg.
+  // else this will become e
+  let handleClick3 = (p1) => {
+    console.log("Click 3", p1);
+  };
+
+  let handleClick4 = (e, p1) => {
+    console.log(e, p1);
   };
 
   return (
     <div>
-      <h1>
-        Counter Demo {counter1} / {counter}
-      </h1>
-      <input type="button" value="INC" onClick={handleIncrment} />
+      <h1>Hello Event Binding</h1>
+      <input type="button" value="Option1" onClick={handleClick1} />
+      <input type="button" value="Option2" onClick={handleClick2} />
+
+      <input
+        type="button"
+        value="Option3"
+        onClick={() => handleClick3(project)}
+      />
+
+      <input
+        type="button"
+        value="Option4"
+        onClick={(e) => handleClick4(e, project)}
+      />
     </div>
   );
 }
