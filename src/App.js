3772563da9b2c0579225c4d296e67e16505b0684
santoshmondal <santosh.ece06@gmail.com>
2023-03-26 18:09:28 +0530

    workign with

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ function App() {
     <div>
       <h1>List Assignment</h1>
       <ListDemo2 />
+
+      <ConditionalDemo />
     </div>
   );
 }
 
 function ListDemo2() {
   let [city, setCity] = useState("");
+  let [cityValidation, setCityValidatoin] = useState(false);
   let [list, setList] = useState(["delhi", "calcutta"]);
 
   const inputCityHandler = (e) => {
@@ -25,7 +28,10 @@ function ListDemo2() {
 
   const addNewCity = () => {
     if (!city) {
+      setCityValidatoin(true);
       return;
+    } else {
+      setCityValidatoin(false);
     }
 
     let newlist = [city, ...list];
@@ -37,20 +43,49 @@ function ListDemo2() {
 
   return (
     <div>
-      <input
-        type="text"
-        placeholder="Enter city..."
-        value={city}
-        onChange={inputCityHandler}
-        onKeyUp={inputSubmitHandler}
-      />
-      <input type="button" value="Add New City" onClick={addNewCity} />
+      <div>
+        <input
+          type="text"
+          placeholder="Enter city..."
+          value={city}
+          onChange={inputCityHandler}
+          onKeyUp={inputSubmitHandler}
+        />
+        <input type="button" value="Add New City" onClick={addNewCity} />
+      </div>
+      {cityValidation && <small>*City is not valid</small>}
 
       <ul>
         {list.map((item, index) => (
           <li key={index}>2.{item}</li>
         ))}
       </ul>
+    </div>
+  );
+}
+
+function ConditionalDemo() {
+  const [morning, setMorning] = useState(false);
+  const toggleMorningMessage = () => {
+    let newMorning = !morning;
+
+    // RERENDER
+    setMorning(newMorning);
+  };
+
+  return (
+    <div>
+      {morning ? (
+        <div>
+          <h1>Good Morning</h1>{" "}
+        </div>
+      ) : (
+        <h1>Good Evening</h1>
+      )}
+
+      {morning && <h1>Good Morning</h1>}
+
+      <input type="button" value="Toggle" onClick={toggleMorningMessage} />
     </div>
   );
 }
