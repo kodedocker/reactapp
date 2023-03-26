43ac0e03e4a9af29db2478e82e289179b9a69c20
santoshmondal <santosh.ece06@gmail.com>
2023-03-26 17:55:39 +0530

    list input

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ function App() {
     <div>
       <h1>List Assignment</h1>
       <ListDemo2 />
-      <ListDemo1 />
-      <ListDemo />
     </div>
   );
 }
@@ -51,64 +49,6 @@ function ListDemo2() {
       <ul>
         {list.map((item, index) => (
           <li key={index}>2.{item}</li>
-        ))}
-      </ul>
-    </div>
-  );
-}
-
-function ListDemo1() {
-  let [city, setCity] = useState("");
-  let [list, setList] = useState(["delhi", "calcutta"]);
-
-  const inputCityHandler = (e) => {
-    setCity(e.target.value);
-  };
-
-  const addNewCity = () => {
-    let newlist = [city, ...list];
-    setList(newlist);
-
-    // clear the box
-    setCity("");
-  };
-
-  return (
-    <div>
-      <input
-        type="text"
-        placeholder="Enter city..."
-        value={city}
-        onChange={inputCityHandler}
-      />
-      <input type="button" value="Add New City" onClick={addNewCity} />
-
-      <ul>
-        {list.map((item, index) => (
-          <li key={index}>1.{item}</li>
-        ))}
-      </ul>
-    </div>
-  );
-}
-
-// JSX :: list of string
-function ListDemo() {
-  let [list, setList] = useState(["delhi", "calcutta"]);
-
-  const addNewCity = () => {
-    let newlist = [...list, "Mumbai"];
-    setList(newlist);
-  };
-
-  return (
-    <div>
-      <hr />
-      <input type="button" value="Add New City" onClick={addNewCity} />
-
-      <ul>
-        {list.map((item, index) => (
-          <li key={index}>{item}</li>
         ))}
       </ul>
     </div>
