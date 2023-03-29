ce3c5dfc05a0b4a4848c606a963f77079a88d8b5
santoshmondal <santosh.ece06@gmail.com>
2023-03-29 10:47:18 +0530

    stylig and themeing.

--- src/App.js
+++ src/App.js
@@ -9,16 +9,57 @@ function App() {
 }
 
 function BootstrapStyling() {
+  let [primary, setPrimary] = useState("dark");
+  let [secondary, setSecondary] = useState("light");
   let [list] = useState([1, 11, 11, 11, 1, 1, 1, 11]);
+
+  let themeHandler = (primary, secondary) => {
+    primary = primary || "dark";
+    secondary = secondary || "light";
+
+    setPrimary(primary);
+    setSecondary(secondary);
+  };
 
   return (
     <div>
-      <h1 className="bg-primary text-light p-3 mb-1 sticky-top">
+      <h1 className={`bg-${primary} text-${secondary} p-3 mb-1 sticky-top`}>
         Hello Bootstrap
       </h1>
 
+      <input
+        type="button"
+        value="P"
+        className="btn btn-sm btn-primary"
+        onClick={() => themeHandler("primary")}
+      />
+      <input
+        type="button"
+        value="P"
+        className="btn btn-sm btn-secondary"
+        onClick={() => themeHandler("secondary")}
+      />
+      <input
+        type="button"
+        value="P"
+        className="btn btn-sm btn-info"
+        onClick={() => themeHandler("info")}
+      />
+      <input
+        type="button"
+        value="P"
+        className="btn btn-sm btn-warning"
+        onClick={() => themeHandler("warning")}
+      />
+      <input
+        type="button"
+        value="P"
+        className="btn btn-sm btn-danger"
+        onClick={() => themeHandler("danger")}
+      />
+
       {list.map((item, index) => (
-        <div key={index} className="alert alert-primary mb-1">
+        <div key={index} className={`alert alert-${primary} mb-1`}>
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius,
           distinctio laboriosam. Ab natus tenetur consequuntur, at beatae
           laborum aperiam reprehenderit maxime libero, obcaecati incidunt
