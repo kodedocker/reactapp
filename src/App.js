021fb3a1483fcc3d3d1482fadde67eabaa760518
santoshmondal <santosh.ece06@gmail.com>
2023-03-29 11:03:59 +0530

    routings...with componetns.

--- src/App.js
+++ src/App.js
@@ -3,18 +3,51 @@ import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <BrowserRouter>
-      <div>
-        <Link to={"/page1"}>Page1 |</Link>
-        <Link to={"/page2"}>Page2 |</Link>
-        <Link to={"/page3"}>Page3 |</Link>
-      </div>
+      <AppNav />
       <Routes>
-        <Route path="/" element={<h1>Page1</h1>} />
-        <Route path="/page1" element={<h1>Page1</h1>} />
-        <Route path="/page2" element={<h1>Page2</h1>} />
-        <Route path="/page3" element={<h1>Page3</h1>} />
+        <Route path="/" element={<Page1 />} />
+        <Route path="/page1" element={<Page1 />} />
+        <Route path="/page2" element={<Page2 />} />
+        <Route path="/page3" element={<Page3 />} />
       </Routes>
     </BrowserRouter>
+  );
+}
+
+function AppNav() {
+  return (
+    <div>
+      <Link to={"/page1"}>Page1 |</Link>
+      <Link to={"/page2"}>Page2 |</Link>
+      <Link to={"/page3"}>Page3 |</Link>
+    </div>
+  );
+}
+
+function Page1() {
+  return (
+    <div>
+      <h1 className="bg-primary">Page1</h1>
+      <p>lorem</p>
+    </div>
+  );
+}
+
+function Page2() {
+  return (
+    <div>
+      <h1 className="bg-info">Page2</h1>
+      <p>lorem</p>
+    </div>
+  );
+}
+
+function Page3() {
+  return (
+    <div>
+      <h1 className="bg-danger">Page3</h1>
+      <p>lorem</p>
+    </div>
   );
 }
 
