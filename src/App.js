4fbc3d5548c05462c4e0cc39e7da36a9ed80a5f2
santoshmondal <santosh.ece06@gmail.com>
2023-03-29 11:00:52 +0530

    basic of routing.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
+      <div>
+        <Link to={"/page1"}>Page1 |</Link>
+        <Link to={"/page2"}>Page2 |</Link>
+        <Link to={"/page3"}>Page3 |</Link>
+      </div>
       <Routes>
         <Route path="/" element={<h1>Page1</h1>} />
         <Route path="/page1" element={<h1>Page1</h1>} />
