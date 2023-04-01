3bdb04ee8e94a95a1abccb2ef6cd0b5a0d40e70d
santoshmondal <santosh.ece06@gmail.com>
2023-04-01 19:29:36 +0530

    hover, custom, arbitrary color.

--- src/routingcomp/Page2.js
+++ src/routingcomp/Page2.js
@@ -1,8 +1,16 @@
 function Page2() {
   return (
     <div>
-      <h1 className="bg-info">Page2</h1>
-      <p>lorem</p>
+      <h1 className="bg-blue-600 text-white p-4 text-2xl hover:bg-blue-800">
+        Page2
+      </h1>
+      <h1 className="bg-[#faa] text-2xl p-4">Hello World</h1>
+
+      <input
+        type="button"
+        value="Signup"
+        className="bg-teal-600 text-white text-2xl rounded-md mt-1 p-2 hover:bg-orange-400 hover:cursor-pointer"
+      />
     </div>
   );
 }
