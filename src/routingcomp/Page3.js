cc1ccf35fe406591646e94097f22de865fb6bfda
santoshmondal <santosh.ece06@gmail.com>
2023-04-01 19:48:29 +0530

    tailwind like bootstrap.

--- src/routingcomp/Page3.js
+++ src/routingcomp/Page3.js
@@ -1,8 +1,13 @@
 function Page3() {
   return (
     <div>
-      <h1 className="bg-danger">Page3</h1>
-      <p>lorem</p>
+      <h1 className="bg-primary-800 text-2xl text-white p-4">Page3</h1>
+      <p className="bg-primary-100 text-secondary-800 text-lg p-3">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque
+        voluptates repellendus corrupti. Nulla alias vero, voluptate, facere eos
+        impedit error eligendi eaque adipisci, modi non quisquam ad inventore
+        enim. Odit.
+      </p>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
 
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        custom: "#abcdef",
+        primary: colors.purple,
+        secondary: colors.gray,
+        info: colors.indigo,
+        success: colors.green,
+      },
+    },
   },
   plugins: [],
 };
